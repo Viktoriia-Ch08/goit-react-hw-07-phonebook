73de60c4b181747a319f252682c60d51ff1fdebb
Viktoriia Cherkashyna <vikushacherk98@gmail.com>
2023-10-26 11:30:03 +0300

    first commit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,28 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import {
-  persistStore,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-
 import filterSlice from './filterSlice';
-import { persistedReducer } from './contactsSlice';
+import contactsSlice from './contactsSlice';
 
 export const store = configureStore({
   reducer: {
-    contacts: persistedReducer,
+    contacts: contactsSlice,
     filter: filterSlice,
   },
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
 });
-
-export const persistor = persistStore(store);
